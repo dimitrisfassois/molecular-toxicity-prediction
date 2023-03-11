eef712544d4fe8ad6b9549bc4aca83d1eedb0d84
Demetrios Fassois <dimitris.fasois@gmail.com>
2023-03-11 17:24:05 -0500

    Modified model_evalution.py: Added weight in computing AUC score.

--- trainer/model_evaluation.py
+++ trainer/model_evaluation.py
@@ -5,12 +5,13 @@ import deepchem as dc
 
 from util.constants import CONST
 
-def evaluate_model(model, val_generator, val_dataset):
+def evaluate_model(dc_model, val_generator, val_dataset):
     y_true = val_dataset.y
+    w = val_dataset.w
     n_tasks = len(CONST.TASKS)
-    y_pred = model.predict_on_generator(val_generator)
+    y_pred = dc_model.predict_on_generator(val_generator)
     y_pred = y_pred[:val_dataset.y.shape[0]]
     metric = dc.metrics.roc_auc_score
     for i in range(n_tasks):
-        score = metric(dc.metrics.to_one_hot(y_true[:, i]), y_pred[:, i])
+        score = metric(dc.metrics.to_one_hot(y_true[:, i]), y_pred[:, i], sample_weight=w[:, i])
         logging.info(f"Task: {CONST.TASKS[i]}, score: {score}")
